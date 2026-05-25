8b04ea5e161b63b438125c0683233fcaea18e385
nikhil goyal <nikhil.goyal@mercedes-benz.com>
2026-05-25 09:48:51 +0100

    Init

---
+++ src/main/java/com/spring/xpose/SpringXposeApplication.java
@@ -0,0 +1,13 @@
+package com.spring.xpose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringXposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringXposeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/xpose/SpringXposeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.xpose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringXposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
